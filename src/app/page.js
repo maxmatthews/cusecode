991e130b5fba85251f20079c5317fc8b8ba25080
Max Matthews <max@mzmtech.com>
2023-06-11 20:49:14 -0400

    Fix page background and text color not loading

--- src/app/page.js
+++ src/app/page.js
@@ -6,29 +6,31 @@ import Footer from "@/components/Footer";
 
 export default function Home() {
 	return (
-		<main className={`container ${styles.main}`}>
-			<div style={{ textAlign: "center" }}>
-				<Image
-					style={{ marginTop: 50 }}
-					src={"/logo.svg"}
-					alt={"cuse code logo"}
-					width={950 / 2}
-					height={742 / 2}
-					priority={true}
-				></Image>
-			</div>
-			<h2 style={{ marginTop: 50 }}>
-				Cuse Code: We're a dev shop powered by junior developers fulfilling the
-				entire <strong>SDLC</strong>.
-			</h2>
-			<h4 style={{ marginTop: 40, marginBottom: 40 }}>
-				If you've made it to our site, you've probably got some questions.
-				Hopefully we've got some answers. Click on <strong>any</strong> question
-				below to learn more.
-			</h4>
-			<FAQ />
-			<Contact />
-			<Footer />
-		</main>
+		<div className={styles.wrapper}>
+			<main className={`container`}>
+				<div style={{ textAlign: "center" }}>
+					<Image
+						style={{ marginTop: 50 }}
+						src={"/logo.svg"}
+						alt={"cuse code logo"}
+						width={950 / 2}
+						height={742 / 2}
+						priority={true}
+					></Image>
+				</div>
+				<h2 style={{ marginTop: 50 }}>
+					Cuse Code: We're a dev shop powered by junior developers fulfilling
+					the entire <strong>SDLC</strong>.
+				</h2>
+				<h4 style={{ marginTop: 40, marginBottom: 40 }}>
+					If you've made it to our site, you've probably got some questions.
+					Hopefully we've got some answers. Click on <strong>any</strong>{" "}
+					question below to learn more.
+				</h4>
+				<FAQ />
+				<Contact />
+				<Footer />
+			</main>
+		</div>
 	);
 }
